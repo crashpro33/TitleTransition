3ba12a8c4039e802812a2ddd669755152585d18d
Matthew King <32280053+crashpro33@users.noreply.github.com>
2019-04-30 19:10:26 -0500

    Create TitleTransition.cs

---
+++ TitleTransition.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titlePress : MonoBehaviour
+{
+
+    public Canvas titleScreen;
+    public Canvas mainMenu;
+    bool buttonPressed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && !buttonPressed)
+        {
+            buttonPressed = true;
+            StartCoroutine(DoFade());
+            StartCoroutine(FadeIn());
+        }  
+    }
+
+    IEnumerator DoFade()
+    {
+        
+        CanvasGroup canvasGroup = titleScreen.GetComponent<CanvasGroup>();
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime / 2;
+            yield return null;
+        }
+        titleScreen.gameObject.SetActive(false);
+        yield return null;
+    }
+
+    IEnumerator FadeIn()
+    {
+        CanvasGroup canvasGroup = mainMenu.GetComponent<CanvasGroup>();
+        canvasGroup.interactable = true;
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += Time.deltaTime / 2;
+            yield return null;
+        }
+        yield return null;
+    }
+
+}
